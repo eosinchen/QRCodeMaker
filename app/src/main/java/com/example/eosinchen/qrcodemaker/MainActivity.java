85e2b39d171893439c74721df247248d51f8bb02
Eosin Chen <eosinchen@gmail.com>
2017-12-08 00:19:13 +0800

    初始化

---
+++ app/src/main/java/com/example/eosinchen/qrcodemaker/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.eosinchen.qrcodemaker;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onBtnMakeQRCodeClick(View v)
+    {
+        // QR code 的內容
+        String QRCodeContent;
+        // QR code 寬度
+        int QRCodeWidth = 200;
+        // QR code 高度
+        int QRCodeHeight = 200;
+
+        //
+        EditText edtQRCode = (EditText) findViewById(R.id.edtQRCode);
+        QRCodeContent = edtQRCode.getText().toString();
+
+        if(QRCodeContent.isEmpty()) {
+            QRCodeContent = "This is sample";
+        }
+
+        // QR code 內容編碼
+        Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        MultiFormatWriter writer = new MultiFormatWriter();
+        try
+        {
+            // 容錯率姑且可以將它想像成解析度，分為 4 級：L(7%)，M(15%)，Q(25%)，H(30%)
+            // 設定 QR code 容錯率為 H
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+            // 建立 QR code 的資料矩陣
+            BitMatrix result = writer.encode(QRCodeContent, BarcodeFormat.QR_CODE, QRCodeWidth, QRCodeHeight, hints);
+            // ZXing 還可以生成其他形式條碼，如：BarcodeFormat.CODE_39、BarcodeFormat.CODE_93、BarcodeFormat.CODE_128、BarcodeFormat.EAN_8、BarcodeFormat.EAN_13...
+
+            //建立點陣圖
+            Bitmap bitmap = Bitmap.createBitmap(QRCodeWidth, QRCodeHeight, Bitmap.Config.ARGB_8888);
+            // 將 QR code 資料矩陣繪製到點陣圖上
+            for (int y = 0; y<QRCodeHeight; y++)
+            {
+                for (int x = 0;x<QRCodeWidth; x++)
+                {
+                    bitmap.setPixel(x, y, result.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+
+            ImageView imgView = (ImageView) findViewById(R.id.imgQRCode);
+            // 設定為 QR code 影像
+            imgView.setImageBitmap(bitmap);
+        }
+        catch (WriterException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
